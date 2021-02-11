5bead066897007026315ae3b50ec21cb679bfc6c
Daniel Tweedie <djtwd.ix@icloud.com>
2021-02-11 19:09:00 +0000

    Refactored code into a function called fetchBreedDescription and fixed URL link so it doesn't throw an error

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,21 +3,27 @@ const breedGiven = process.argv.splice(2).join("");
 //require request
 const request = require('request');
 
-//send get request to cat API with breedGiven as search query
-request(`https://api.thectapi.com/v1/breeds/search?q=${breedGiven}`, (err, response, body) => {
-  //if error throw error
-  if (err) {
-    console.log(err);
-    process.exit();
-  } else {
-    //parse body to turn into object
-    const data = JSON.parse(body);
-    //if there is nothing in data print breed is not in database
-    if (!data[0]) {
-      console.log("We're sorry this breed is not in our database or does not exist, please try another breed");
-    //if there is something in data print data
+const fetchBreedDescription = (breedName, callback) => {
+  //send get request to cat API with breedGiven as search query
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedGiven}`, (err, response, body) => {
+    //if error print error to console and exit
+    if (err) {
+      console.log(err);
+      process.exit();
     } else {
-      console.log(data);
+      //parse body to turn into object
+      const data = JSON.parse(body);
+      //if there is nothing in data print breed is not in database
+      if (!data[0]) {
+        console.log("We're sorry this breed is not in our database or does not exist, please try another breed");
+        //if there is something in data print data
+      } else {
+        console.log(data);
+      }
     }
-  }
-});
+  });
+}
+
+fetchBreedDescription();
+
+module.exports = { fetchBreedDescription };
